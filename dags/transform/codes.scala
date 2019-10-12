ed3ab7b0b421720e5f35a09bd8da4d9e52716c65
Ammar <ammar.rna@gmail.com>
2019-10-12 07:18:58 +0500

    updated yaml and scala ETl

--- dags/transform/codes.scala
+++ dags/transform/codes.scala
@@ -1,3 +1,7 @@
-val state_df = spark.read.format("csv").option("header", "true").option("inferschema", "true").load("s3://<s3-bucket>/state_code.csv.csv")
+val state_df = spark.read.format("csv").option("header", "true").option("inferschema", "true").load("s3://<s3-bucket>/state_codecsv")
 
 state_df.write.mode("overwrite").parquet("s3://<s3-bucket>/lake/state_dim/")
+
+val country_df = spark.read.format("csv").option("header", "true").option("inferschema", "true").load("s3://<s3-bucket>/country_code.csv")
+
+country_df.write.mode("overwrite").parquet("s3://<s3-bucket>/lake/country_dim/")
